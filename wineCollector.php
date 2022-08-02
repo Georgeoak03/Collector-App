5d9c8193a7443501545f143aec7fd4eab0305aad
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2022-08-02 13:46:36 +0100

    added table

--- wineCollector.php
+++ wineCollector.php
@@ -7,6 +7,42 @@ $db = new PDO($connectionString, $dbUsername, $dbPassword);
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-echo 'hello';
-//$allResults = $query->fetchAll();
+$queryString = 'SELECT * FROM `wines` ORDER BY `date` DESC';
+$query = $db->prepare($queryString);
+$query->execute();
+
+$allResults = $query->fetchAll();
 //print_r($allResults);
+
+?>
+
+<table>
+    <thead>
+    <tr>
+        <th>Wines</th>
+        <th>Producer</th>
+        <th>Region</th>
+        <th>Date</th>
+        <th>Location Drank</th>
+        <th>Colour</th>
+        <th>Vintage</th>
+        <th>Price</th>
+        <th>Rating</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr>
+        <?php foreach ($allResults as $allResult){ ?>
+        <td> <?php echo $allResult['wine']; ?> </td>
+        <td> <?php echo $allResult['producer']; ?> </td>
+        <td> <?php echo $allResult['region']; ?> </td>
+        <td> <?php echo $allResult['date']; ?> </td>
+        <td> <?php echo $allResult['drinking_location']; ?> </td>
+        <td> <?php echo $allResult['colour']; ?> </td>
+        <td> <?php echo $allResult['vintage']; ?> </td>
+        <td> <?php echo $allResult['price_gbp']; ?> </td>
+        <td> <?php echo $allResult['rating']; ?> </td>
+    </tr>
+    <?php } ?>
+    </tbody>
