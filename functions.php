7209b14ea5aa1fcd4edf7d0aaadc921d2b360a79
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2022-08-04 12:13:08 +0100

    made review changes

--- functions.php
+++ functions.php
@@ -2,14 +2,16 @@
 
 <?php
 
-function dbpull() {$connectionString = 'mysql:host=db; dbname=winecollector';
+function dbpull()
+    {
+        $connectionString = 'mysql:host=db; dbname=winecollector';
     $dbUsername = 'root';
     $dbPassword = 'password';
     $db = new PDO($connectionString, $dbUsername, $dbPassword);
 
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    $queryString = 'SELECT * FROM `wines` ORDER BY `date` DESC';
+    $queryString = 'SELECT `wine`, `date`, `drinking_location`, `colour`, `producer`, `region`, `vintage`, `price_gbp`, `rating` FROM `wines` ORDER BY `date` DESC';
     $query = $db->prepare($queryString);
     $query->execute();
 
@@ -36,8 +38,3 @@ function displayWine(array $wine): string {
         '<tr><td class="wine">' . $wine['wine'] . '</td><td class="wine-content"> ' . $wine['date'] . '</td><td class="wine-content"> ' . $wine['drinking_location'] . '</td><td class="wine-content"> ' . $wine['colour'] . '</td><td class="wine-content"> ' . $wine['producer'] . '</td><td class="wine-content"> ' . $wine['region'] . '</td><td class="wine-content"> ' . $wine['vintage'] . '</td><td class="wine-content"> ' . $wine['price_gbp'] . '</td><td class="wine-content"> ' . $wine['rating'] . '</td></tr>';
     return $wineHtml;
 }
-
-
-
-
-
